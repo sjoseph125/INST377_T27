0722d18f057fca78a3413cc0631503d44cff32d4
Samson Joseph <sjosep45@terpmail.umd.edu>
2020-11-10 21:04:07 -0500

    added react.js

--- public/script.js
+++ public/script.js
@@ -1,17 +1,27 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
 // const api=0e31f29705fa26f604e00f070aea5e11
+const state = 'AR';
 const api_url = 'http://www.opensecrets.org/api/?method=getLegislators&id=AR&output=json&apikey=0e31f29705fa26f604e00f070aea5e11';
-let leg=[];
+const leg = [];
 async function getData() {
   const responce = await fetch(api_url);
   const data = await responce.json();
   // leg.push(data)
-  leg.push(data.response.legislator)
-    // [0]['@attributes'].firstlast);
-//   return leg
-  console.log(leg);
+  leg.push(data.response.legislator[0]['@attributes'].firstlast);
+  // return leg
+  console.log(leg[0]);
 }
 
-data = getData();
+getData();
+console.log(leg);
+console.log(leg[0]);
+
+
+
+
+
+
 //   .then(rep => rep.json())
 
 // console.log(leg)
@@ -24,7 +34,7 @@ data = getData();
 //   }
 // })
 //   .then((fromServer) => fromServer.json())
-    
+
 //   .then((jsonFromServer) => restaurants.push(...jsonFromServer))
 // console.log(restaurants)
 //   .catch((err) => {
