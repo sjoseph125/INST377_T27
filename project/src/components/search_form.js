337d67d914e2eb5be5b86bb9f6272eff180372e5
Samson Joseph <sjosep45@terpmail.umd.edu>
2020-12-14 13:14:04 -0500

    Rearranged some files
    Added the iSchool logo as well

--- project/src/components/search_form.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function Search(){
-    return (<form className="search_form">
-    <input type="text" className="search" placeholder="Search For A State"/>
-    </form>)
-}
-export default Search
--- server.js
+++ server.js
@@ -21,18 +21,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.route('/api')
-  .get((req, res) => {
-    console.log('GET request detected');
-
-    res.send('Assignment 2 Group 67');
-  })
-  .post(async(req, res) => {
-    const data = await fetch('http://www.opensecrets.org/api/?method=getLegislators&id=NJ&output=json&apikey=0e31f29705fa26f604e00f070aea5e11');
-    const json = await data.json();
-    res.json(json);
-    console.log(json)
-  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
